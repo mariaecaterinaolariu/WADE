cd43f65753379ef129de9a7e164099b1314036a9
Maria Ecaterina Olariu <mariaecaterina.olariu@gmail.com>
2024-02-05 01:31:33 +0200

    Update app.py

--- wade-project/backend/wade-imp/app.py
+++ wade-project/backend/wade-imp/app.py
@@ -174,5 +174,5 @@ def filter_query():
 
     return jsonify({'images': image_filenames})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
